0a62a041c2e15ec927c8d6ad01a64bd5b428f5bb
Krmjn09 <kmahajan1_be22@thapar.edu>
2024-07-07 14:17:24 +0530

    COMMITED

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,3 +1,27 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div className=""></div>
+  return (
+    <div
+      className="w-screen h-screen bg-black flex justify-center items-center
+    text-white "
+    >
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The Best Journal App,Period.</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          Welcome to Chronicles, your personal digital sanctuary for capturing
+          thoughts, experiences, and moments that matter. Our intuitive and
+          user-friendly platform empowers you to effortlessly document your
+          lifes journey. Get started today!
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
